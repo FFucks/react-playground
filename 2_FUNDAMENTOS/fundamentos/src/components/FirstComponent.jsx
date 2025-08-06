c8888b0547b6221ef929283cf32ce575e96b0395
FFucks <fabio_geiss@hotmail.com>
2025-08-06 13:37:24 -0300

    novos componentes

---
+++ 2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.jsx
@@ -0,0 +1,22 @@
+// arquivos de estilo
+
+
+
+const FirstComponent = () => {
+
+    //essa funçao adiciona o componente
+    /*
+        Comentario de multiplas linhas
+    */
+
+    return (
+        <div>
+            {/* Comentario do React*/}
+
+            <h1>Meu primeiro componente</h1>
+            <p className="teste">Meu texto</p>
+        </div>
+    );
+};
+
+export default FirstComponent;
